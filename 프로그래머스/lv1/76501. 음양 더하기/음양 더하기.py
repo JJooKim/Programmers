c3bd32db57b8a4f85facf9a30ac1db78a18af4a1
JJooKim <122198271+JJooKim@users.noreply.github.com>
2023-09-07 13:25:38 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,11 @@
+
+def solution(absolutes, signs):
+#     result = 0
+    
+#     for i in range(len(absolutes)):
+#         if signs[i]:
+#             result += absolutes[i]
+#         else:
+#             result -= absolutes[i]
+#     return result
+    return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
